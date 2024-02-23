1caf798a116b3574515f52ceb94aa26965b69b0e
jakeevans <ejake370@gmail.com>
2024-02-23 15:30:53 -0700

    Updated default values for ADSR

--- src/utils/Store.jsx
+++ src/utils/Store.jsx
@@ -91,10 +91,10 @@ export default function Store(props) {
       type: filter.type,
     },
     envelope: {
-      attack: 0.2,
+      attack: 0,
       decay: 0.1,
-      sustain: 0.6,
-      release: 0.3,
+      sustain: 0.3,
+      release: 0.5,
     },
   });
   return <CTX.Provider value={stateHook}>{props.children}</CTX.Provider>;
